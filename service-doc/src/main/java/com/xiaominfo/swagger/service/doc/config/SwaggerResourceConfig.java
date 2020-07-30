6e05339fbe3357bb399dfa4d7e65f661c495ad01
liangtf <liangtengfei@wandoutech.com>
2020-07-30 19:12:14 +0800

    zuul 结合 knife4j

---
+++ service-doc/src/main/java/com/xiaominfo/swagger/service/doc/config/SwaggerResourceConfig.java
@@ -0,0 +1,52 @@
+package com.xiaominfo.swagger.service.doc.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ *
+ * @since:swagger-bootstrap-ui 1.0
+ * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a> 
+ * 2019/05/04 12:38
+ */
+@Component
+@Primary
+public class SwaggerResourceConfig implements SwaggerResourcesProvider {
+
+    Logger logger= LoggerFactory.getLogger(SwaggerResourceConfig.class);
+
+
+    @Autowired
+    private RouteLocator routeLocator;
+
+    @Override
+    public List<SwaggerResource> get() {
+        //获取所有router
+        List<SwaggerResource> resources = new ArrayList<>();
+        List<Route> routes = routeLocator.getRoutes();
+        logger.info("Route Size:{}", routes.size());
+        for (Route route:routes) {
+            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs")));
+        }
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        logger.info("name:{},location:{}", name, location);
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;
+    }
+}
---
+++ service-user/src/main/java/com/xiaominfo/swagger/service/user/config/SwaggerConfiguration.java
@@ -0,0 +1,94 @@
+
+package com.xiaominfo.swagger.service.user.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean(value = "userApi")
+    @Order(value = 1)
+    public Docket groupRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xiaominfo.swagger.service.user.controller"))
+                .paths(PathSelectors.any())
+                        // 注意这里哦
+                .build().securityContexts(Lists.newArrayList(securityContext()))
+                        .securitySchemes(Lists.<SecurityScheme>newArrayList(apiKey()));
+    }
+
+    private ApiInfo groupApiInfo(){
+        Contact contact = new Contact("作者姓名", "地址", "邮箱");
+        return new ApiInfoBuilder()
+                // 标题
+                .title("apis")
+                // 描述
+                .description("apis")
+                // 服务url
+                .termsOfServiceUrl("/")
+                // 版本
+                .version("1.0")
+                // 联系
+                .contact(contact)
+                .build();
+    }
+
+
+
+    private ApiKey apiKey() {
+        // name ： 参数名称 keyName：是header中的key
+        return new ApiKey("BearerToken", "token", "header");
+    }
+    private ApiKey apiKey1() {
+        return new ApiKey("BearerToken1", "Authorization-x", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+    private SecurityContext securityContext1() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth1())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("BearerToken", authorizationScopes));
+    }
+    List<SecurityReference> defaultAuth1() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("BearerToken1", authorizationScopes));
+    }
+
+}
